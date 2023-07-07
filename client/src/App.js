7eddb38adce2a66b6525abb29c3a70bb7d6cf1f4
Mike Heavers <mheavers@gmail.com>
2023-07-07 09:46:35 -0700

    receive and deliver podcast index api results

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [podcastName, setPodcastName] = React.useState("");
 
   React.useEffect(() => {
     fetch("/api")
@@ -12,11 +13,41 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  const handlePodcastSearch = (e) => {
+    e.preventDefault();
+    console.log("podcastName", podcastName);
+    fetch("/api/searchForPodcast", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ podcastName }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <div className="formContainer">
+        <form onSubmit={handlePodcastSearch} method="POST">
+          <label htmlFor="name">Podcast:</label>
+          <input
+            type="text"
+            id="podcast"
+            name="podcast"
+            onChange={(e) => {
+              setPodcastName(e.target.value);
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const crypto = require("crypto");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
@@ -11,6 +17,49 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Connected to Server" });
+});
+
+app.post("/api/searchForPodcast", async (req, res) => {
+  console.log(req.body);
+  const { podcastName } = req.body;
+  const apiKey = process.env.PODCAST_INDEX_API_KEY;
+  const apiSecret = process.env.PODCAST_INDEX_API_SECRET;
+  const apiHeaderTime = Math.floor(Date.now() / 1000);
+  const sha1Algorithm = "sha1";
+  const sha1Hash = crypto.createHash(sha1Algorithm);
+  const data4Hash = apiKey + apiSecret + apiHeaderTime;
+  sha1Hash.update(data4Hash);
+  const hash4Header = sha1Hash.digest("hex");
+  // console.log(`hash4Header=[${hash4Header}]`);
+
+  const options = {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Auth-Date": "" + apiHeaderTime,
+      "X-Auth-Key": apiKey,
+      Authorization: hash4Header,
+      "User-Agent": "Podquest/1.0",
+    },
+  };
+
+  const url =
+    "https://api.podcastindex.org/api/1.0/search/byterm?q=" +
+    podcastName +
+    "&max=10";
+  await axios.get(url, options).then((response) => {
+    // console.log(response.data);
+    const feeds = response.data.feeds;
+    const episodes = [];
+    feeds.forEach((feed) => {
+      episodes.push({
+        title: feed.title,
+        url: feed.url,
+      });
+    });
+    console.log(episodes);
+    res.json({ episodes });
+  });
 });
 
 // All other GET requests not handled before will return our React app
